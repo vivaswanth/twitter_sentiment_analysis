1866547458ea72d3d4cfbc0b8b5b95b7a8cab308
vivaswanth vidya katravulapalli <vivaswanth.k@gmail.com>
2017-11-12 08:53:45 +0530

    tweet

---
+++ main.py
@@ -0,0 +1,149 @@
+import tweepy           # To consume Twitter's API
+import pandas as pd     # To handle data
+import numpy as np  
+# For number computing
+
+from IPython.display import display
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'NgBRtzywsQ9yrfVAWnjjt1m3K'
+CONSUMER_SECRET = 'mqyQpQnhJn1sH3IOhrMo5MwrELmsHwRE4aJ32fLQQyGLu1ewzg'
+
+# Access:
+ACCESS_TOKEN  = '2251776488-qhn2k2IpgSOHWqfVI0MUgLu27RTn0TkN2ws1ptv'
+ACCESS_SECRET = 'R8j6uyFfD3cGELeeInqh3cBWK8HXBFiefdiIAkSHrSNaI'
+
+from credentials import *    # This will allow us to use the keys as variables
+
+# API's setup:
+def twitter_setup():
+    """
+    Utility function to setup the Twitter's API
+    with our access keys provided.
+    """
+    # Authentication and access using keys:
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+    # Return API with authentication:
+    api = tweepy.API(auth)
+    return api
+
+# We create an extractor object:
+extractor = twitter_setup()
+
+# We create a tweet list as follows:
+tweets = extractor.user_timeline(screen_name="BarackObama", count=200)
+print("Number of tweets extracted: {}.\n".format(len(tweets)))
+
+# We print the most recent 5 tweets:
+print("5 recent tweets:\n")
+for tweet in tweets[:5]:
+    print(tweet.text)
+    print()
+    
+    # We create a pandas dataframe as follows:
+data = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+
+# We display the first 10 elements of the dataframe:
+display(data.head(10))
+
+# Internal methods of a single tweet object:
+print(dir(tweets[0]))
+
+# We print info from the first tweet:
+print(tweets[0].id)
+print(tweets[0].created_at)
+print(tweets[0].source)
+print(tweets[0].favorite_count)
+print(tweets[0].retweet_count)
+print(tweets[0].geo)
+print(tweets[0].coordinates)
+print(tweets[0].entities)
+
+# We add relevant data:
+data['len']  = np.array([len(tweet.text) for tweet in tweets])
+data['ID']   = np.array([tweet.id for tweet in tweets])
+data['Date'] = np.array([tweet.created_at for tweet in tweets])
+data['Source'] = np.array([tweet.source for tweet in tweets])
+data['Likes']  = np.array([tweet.favorite_count for tweet in tweets])
+data['RTs']    = np.array([tweet.retweet_count for tweet in tweets])
+
+
+# Display of first 10 elements from dataframe:
+display(data.head(10))
+
+# We extract the mean of lenghts:
+mean = np.mean(data['len'])
+
+print("The lenght's average in tweets: {}".format(mean))
+
+# We extract the tweet with more FAVs and more RTs:
+
+fav_max = np.max(data['Likes'])
+rt_max  = np.max(data['RTs'])
+
+fav = data[data.Likes == fav_max].index[0]
+rt  = data[data.RTs == rt_max].index[0]
+
+# Max FAVs:
+print("The tweet with more likes is: \n{}".format(data['Tweets'][fav]))
+print("Number of likes: {}".format(fav_max))
+print("{} characters.\n".format(data['len'][fav]))
+
+# Max RTs:
+print("The tweet with more retweets is: \n{}".format(data['Tweets'][rt]))
+print("Number of retweets: {}".format(rt_max))
+print("{} characters.\n".format(data['len'][rt]))
+
+# We create time series for data:
+
+tlen = pd.Series(data=data['len'].values, index=data['Date'])
+tfav = pd.Series(data=data['Likes'].values, index=data['Date'])
+tret = pd.Series(data=data['RTs'].values, index=data['Date'])
+
+from textblob import TextBlob
+import re
+
+def clean_tweet(tweet):
+    '''
+    Utility function to clean the text in a tweet by removing 
+    links and special characters using regex.
+    '''
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def analize_sentiment(tweet):
+    '''
+    Utility function to classify the polarity of a tweet
+    using textblob.
+    '''
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
+    
+    # We create a column with the result of the analysis:
+data['SA'] = np.array([ analize_sentiment(tweet) for tweet in data['Tweets'] ])
+
+# We display the updated dataframe with the new column:
+display(data.head(10))
+
+# We construct lists with classified tweets:
+
+pos_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] > 0]
+neu_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] == 0]
+neg_tweets = [ tweet for index, tweet in enumerate(data['Tweets']) if data['SA'][index] < 0]
+
+# We print percentages:
+
+print("Percentage of positive tweets: {}%".format(len(pos_tweets)*100/len(data['Tweets'])))
+print("Percentage of neutral tweets: {}%".format(len(neu_tweets)*100/len(data['Tweets'])))
+print("Percentage de negative tweets: {}%".format(len(neg_tweets)*100/len(data['Tweets'])))
